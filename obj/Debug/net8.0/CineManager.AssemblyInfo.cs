9d7ded34efe7a6da8ce4e0a9e443fdafdae7b50d
Luis Eduardo Lumi Alzamora <llumialz@emeal.nttdata.com>
2025-08-21 16:23:43 -0500

    Actualizacion

--- obj/Debug/net8.0/CineManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/CineManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CineManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebb4337933581ea59c8f6bb56249954e68411cd6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a34df7783364a899c02740ba2672f0efbf9e678")]
 [assembly: System.Reflection.AssemblyProductAttribute("CineManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CineManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
